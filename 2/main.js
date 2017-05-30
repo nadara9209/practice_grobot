cdbc3e0c6064996b90c357addf9888f87093a963
ks <smile12_27@naver.com>
2017-05-30 18:34:28 +0900

    Add main.js

---
+++ 2/main.js
@@ -0,0 +1,20 @@
+﻿
+function btnA_onClick() {
+    alert("버튼 A 클릭!");
+}
+
+function btnC_onClick() {
+    alert("경식이");
+}
+
+function btnD_onClick() {
+    alert("버튼 D 클릭!");
+}
+
+function btnE_onClick() {
+    alert("버튼 E 클릭!");
+}
+
+function btnH_onClick() {
+    alert("버튼 H 클릭!");
+}
