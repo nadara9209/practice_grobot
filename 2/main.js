5301f9c0852200eb926208963b5ae350b54fdaee
ju8ha <mukpobu@gmail.com>
2017-05-30 18:34:23 +0900

    Add aButton

---
+++ 2/main.js
@@ -0,0 +1,20 @@
+﻿
+function btnA_onClick() {
+    alert("신전떡볶이 매운떡볶이 떡반 엽기떡볶이 다 맛있다. 배고프다. 김주하");
+}
+
+function btnC_onClick() {
+    alert("버튼 C 클릭!");
+}
+
+function btnD_onClick() {
+    alert("버튼 D 클릭!");
+}
+
+function btnE_onClick() {
+    alert("버튼 E 클릭!");
+}
+
+function btnH_onClick() {
+    alert("버튼 H 클릭!");
+}
