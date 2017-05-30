d1020bacf29d8fb0e46ccaad4aac1964bceacc22
SungGyu Park <nadara9209@gmail.com>
2017-05-30 14:15:10 +0900

    Unzip *.tar.gz

---
+++ 2/main.js
@@ -0,0 +1,20 @@
+
+function btnA_onClick() {
+    alert("버튼 A 클릭!");
+}
+
+function btnC_onClick() {
+    alert("버튼 C 클릭!");
+}
+
+function btnD_onClick() {
+    alert("버튼 D 클릭!");
+}
+
+function btnE_onClick() {
+    alert("버튼 E 클릭!");
+}
+
+function btnH_onClick() {
+    alert("버튼 H 클릭!");
+}
