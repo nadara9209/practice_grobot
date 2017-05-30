64d4a80393a22a71d055da62f8bc65db40ba2f4b
Tabbygray <jundol1222@naver.com>
2017-05-30 19:02:07 +0900

    AddbtnD

---
+++ 2/main.js
@@ -0,0 +1,21 @@
+﻿
+function btnA_onClick() {
+    alert("버튼 A 클릭!");
+}
+
+function btnC_onClick() {
+    alert("버튼 C 클릭!");
+}
+
+function btnD_onClick() {
+    alert("아이씐나");
+}
+
+function btnE_onClick() {
+    alert("버튼 E 클릭!");
+}
+
+function btnH_onClick() {
+    alert("버튼 H 클릭!");
+}
+
