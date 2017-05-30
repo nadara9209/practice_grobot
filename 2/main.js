995b7bf64b04159af4f8ed55d6cc3cb4cb11b5ff
HyunAhCho <201602077@cs-cnu.org>
2017-05-30 18:35:42 +0900

    Add main.js

---
+++ 2/main.js
@@ -0,0 +1,24 @@
+﻿
+function btnA_onClick() {
+    alert("버튼 A 클릭!");
+}
+
+function btnB_onClick() {
+    alert("버튼 B 클릭! CHO ");
+}
+
+function btnC_onClick() {
+    alert("버튼 C 클릭!");
+}
+
+function btnD_onClick() {
+    alert("버튼 D 클릭!");
+}
+
+function btnE_onClick() {
+    alert("버튼 E 클릭!");
+}
+
+function btnH_onClick() {
+    alert("버튼 H 클릭!");
+}
